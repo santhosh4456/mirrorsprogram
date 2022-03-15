155a61b5571f6b4c9efeadfe9768a203cf12ba80
santhosh4456 <100783089+santhosh4456@users.noreply.github.com>
2022-03-15 10:33:58 +0530

    Create mirror string assignment.py

---
+++ mirror string assignment.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+a = input("Enter a name:")
+b = ''
+for i in name:
+    print(i[::-1])
+    print(b)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
